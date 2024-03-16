6c4824d4939b2709fd353b60b28fd168ac8f0e4f
dromse <dromasko4@gmail.com>
2024-03-16 21:32:56 +0200

    change completedAt string

--- src/hooks/useTasks/middleware/completedAt.ts
+++ src/hooks/useTasks/middleware/completedAt.ts
@@ -2,7 +2,8 @@ import { Middleware, Task } from "../types";
 import { cleanBody, findByRegex } from "../utils";
 
 const parse = (task: Task): Task => {
-	const regex = /✅ \[\[(\d{4}-\d{2}-\d{2})\|\1\|(\d{2}:\d{2})\]\]/;
+	const regex =
+		/✅ \[\[(\d{4}-\d{2}-\d{2})\|(\d{4}-\d{2}-\d{2} \| \d{2}:\d{2})\]\]/;
 
 	const match = findByRegex(regex, task);
 
@@ -15,7 +16,17 @@ const parse = (task: Task): Task => {
 	return { ...task, completedAt: match[0], body: newBody };
 };
 
-const stringify = (task: Task): string =>
-	task.completedAt ? ` ${task.completedAt}` : "";
+const stringify = (task: Task): string => {
+	const date = new Date();
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0"); // Adding 1 to month because it's zero-based
+	const day = String(date.getDate()).padStart(2, "0");
+	const hours = String(date.getHours()).padStart(2, "0");
+	const minutes = String(date.getMinutes()).padStart(2, "0");
+
+	const completedAt = ` ✅ [[${year}-${month}-${day}|${year}-${month}-${day} | ${hours}:${minutes}]]`;
+
+	return task.completed ? completedAt : "";
+};
 
 export default { parse, stringify } as Middleware;
