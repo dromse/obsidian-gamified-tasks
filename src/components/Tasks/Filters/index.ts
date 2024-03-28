c86b384b6644a99fb9a31d6c4c5f4ac13dbf4914
dromse <dromasko4@gmail.com>
2024-03-28 23:54:53 +0200

    remove completed filter

--- src/components/Tasks/Filters/index.ts
+++ src/components/Tasks/Filters/index.ts
@@ -1,6 +1,5 @@
-import { default as CompletedFilter } from "./CompletedFilter";
 import { default as LimitFilter } from "./LimitFilter";
 import { default as SearchFilter } from "./SearchFilter";
 import { default as StatusFilter } from "./StatusFilter";
 
-export { StatusFilter, CompletedFilter, LimitFilter, SearchFilter };
+export { StatusFilter, LimitFilter, SearchFilter };
