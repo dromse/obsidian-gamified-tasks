e1657f59e2a8ee6a7d7b3632fd417b26bda66329
dromse <dromasko4@gmail.com>
2024-03-28 23:41:06 +0200

    remove completed setting

--- src/SettingTab.ts
+++ src/SettingTab.ts
@@ -1,9 +1,6 @@
+import { StatusKeys } from "@hooks/useTasks/consts";
+import { StatusFilterOption } from "@hooks/useTasks/types";
 import { App, PluginSettingTab, Setting } from "obsidian";
-import {
-	CompletedFilterOption,
-	StatusFilterOption
-} from "./components/Tasks/TaskList";
-import { Statuses } from "./hooks/useTasks/middleware/status";
 import GrindPlugin, { DEFAULT_SETTINGS } from "./main";
 
 /** Class for Setting Tab where user can set default filtering settings for `Grind Manager` */
@@ -32,7 +29,7 @@ export default class GrindSettingTab extends PluginSettingTab {
 
 		const statusFilterOptions: Record<string, string> = {};
 
-		Statuses.forEach((status) => (statusFilterOptions[status] = status));
+		StatusKeys.forEach((status) => (statusFilterOptions[status] = status));
 
 		new Setting(containerEl)
 			.setName("Default status filter")
@@ -43,21 +40,6 @@ export default class GrindSettingTab extends PluginSettingTab {
 					.setValue(this.plugin.settings.statusFilter)
 					.onChange(async (value) => {
 						this.plugin.settings.statusFilter = value as StatusFilterOption;
-						await this.plugin.saveSettings();
-					}),
-			);
-
-		new Setting(containerEl)
-			.setName("Default completed filter")
-			.addDropdown((dropDown) =>
-				dropDown
-					.addOption("all", "all")
-					.addOption("uncompleted", "uncompleted")
-					.addOption("completed", "completed")
-					.setValue(this.plugin.settings.completedFilter)
-					.onChange(async (value) => {
-						this.plugin.settings.completedFilter =
-							value as CompletedFilterOption;
 						await this.plugin.saveSettings();
 					}),
 			);
