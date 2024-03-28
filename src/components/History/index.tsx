f67e6994423f9b87a61067e10a0d42ed2b21172c
dromse <dromasko4@gmail.com>
2024-03-28 23:41:35 +0200

    added pagination

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -1,51 +1,100 @@
-import { useHistory } from "../../hooks";
+import { useHistory } from "@/hooks";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
-type Props = {};
-
-export default function History({}: Props) {
+export default function History() {
 	const { history, balance, isHistoryParsed } = useHistory();
+	const [page, setPage] = useState(0);
+	const [totalPages, setTotalPages] = useState(0);
+	const [pageInput, setPageInput] = useState(String(page + 1));
+
+	useEffect(() => {
+		if (isHistoryParsed === "parsed") {
+			setTotalPages(Math.ceil(history.length / 10));
+		}
+	}, [isHistoryParsed]);
+
+	useEffect(() => {
+		Number(pageInput) > 0 &&
+			(Number(pageInput) <= totalPages ? setPage(Number(pageInput) - 1) : "");
+	}, [pageInput]);
 
 	if (isHistoryParsed === "parsing") {
 		return <div>Parsing...</div>;
 	}
 
 	if (isHistoryParsed === "parsed") {
+		const historyPages = Array.from({ length: totalPages }, (_, i) => i);
+
 		return (
 			<div>
 				<h2>History</h2>
 				<h3>Balance: {balance} coins</h3>
 
+				{historyPages
+					.slice(page === 0 ? page : page - 1, page + 3)
+					.map((i) => (
+						<button
+							onClick={() => setPage(i)}
+							className={i === page ? styles.activePage : ""}
+						>
+							{i + 1}
+						</button>
+					))}
+
+				{page !== totalPages - 1 && (
+					<>
+						<span>...</span>
+						<button
+							onClick={() => setPage(totalPages - 1)}
+							className={totalPages - 1 === page ? styles.activePage : ""}
+						>
+							{totalPages}
+						</button>
+					</>
+				)}
+
+				<div>
+					<label htmlFor="page-input">Page:</label>
+
+					<input
+						type="number"
+						name="page-input"
+						id=""
+						value={pageInput}
+						onChange={(e) => {
+							const value = e.currentTarget.value;
+							if (
+								value === "" ||
+								(Number(value) > 0 && Number(value) <= totalPages)
+							) {
+								setPageInput(value);
+							}
+						}}
+					/>
+				</div>
+
 				<table className={styles.table}>
 					<thead>
 						<tr>
-							<th scope="col">Title</th>
 							<th scope="col">Change</th>
+							<th scope="col">Title</th>
 							<th scope="col">Date</th>
 						</tr>
 					</thead>
 
 					<tbody>
-						{history.map((row) => (
+						{history.slice(page * 10, (page + 1) * 10).map((row) => (
 							<tr>
-								<th scope="row">{row.title}</th>
-								<td>{row.change > 0 ? "+" + row.change : row.change}</td>
-								<td>
-									{row.date.toLocaleTimeString([], {
-										month: "numeric",
-										day: "numeric",
-										year: "numeric",
-										hour: "2-digit",
-										minute: "2-digit",
-										hour12: false,
-									})}
-								</td>
+								<th scope="row">
+									{row.change > 0 ? "+" + row.change : row.change}
+								</th>
+								<td>{row.title}</td>
+								<td>{row.date}</td>
 							</tr>
 						))}
 					</tbody>
 				</table>
-
-				<ul></ul>
 			</div>
 		);
 	}
