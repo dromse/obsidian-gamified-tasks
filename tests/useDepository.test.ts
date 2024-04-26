b1e22124fac1ac5c6cb3bfd1cf5e64dd76830152
dromse <dromasko4@gmail.com>
2024-04-26 17:18:33 +0300

    update mock useHistory to use useState, update tests for it

--- tests/useDepository.test.ts
+++ tests/useDepository.test.ts
@@ -2,17 +2,18 @@ import { DATE_FORMAT } from "@consts";
 import {
 	DepositoryTransactionState,
 	DEPOSITORY_TRANSACTION_STATE,
-	useDepository,
+	useDepository
 } from "@hooks/useDepository";
 import {
 	AddHistoryRowType,
 	HistoryRow,
-	UseHistoryReturn,
+	UseHistoryReturn
 } from "@hooks/useHistory";
 import { act, renderHook } from "@testing-library/react-hooks";
 import moment from "moment";
 import assert from "node:assert";
 import test, { describe } from "node:test";
+import { useState } from "react";
 
 const recalculateBalance = (history: Array<HistoryRow>): number => {
 	return history.reduce((acc, row) => acc + row.change, 0);
@@ -29,29 +30,33 @@ describe("Depository", () => {
 		];
 
 		function mockUseHistoryForStoreTest(): UseHistoryReturn {
-			const history = mockHistoryForStoreTest.slice();
+			const [history, setHistory] = useState(mockHistoryForStoreTest.slice());
+
 			const balance = history.reduce((acc, row) => acc + row.change, 0);
 			const isHistoryParsed = "parsed";
 			const addHistoryRow = async ({
 				title,
 				change,
 			}: AddHistoryRowType): Promise<void> => {
-				history.push({ title, change, date: moment().format(DATE_FORMAT) });
+				setHistory((prev) => [
+					...prev,
+					{ title, change, date: moment().format(DATE_FORMAT) },
+				]);
 			};
 
 			return { historyRows: history, balance, isHistoryParsed, addHistoryRow };
 		}
 
 		test("store to depository middle correct value", async (t) => {
-			const mockHistory = mockUseHistoryForStoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+			const mockHistory = renderHook(() => mockUseHistoryForStoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.store(2);
+				transactionState = await depository.result.current.store(2);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test("must return success state of store transaction", () => {
 				assert.strictEqual(
@@ -61,24 +66,24 @@ describe("Depository", () => {
 			});
 
 			await t.test("depository balance must increase", () => {
-				assert.strictEqual(result.current.balance, 2);
+				assert.strictEqual(depository.result.current.balance, 2);
 			});
 
 			await t.test("history balance must decrease", () => {
-				assert.strictEqual(mockHistory.balance, 3);
+				assert.strictEqual(mockHistory.result.current.balance, 3);
 			});
 		});
 
 		test("store to depository boundary correct value", async (t) => {
-			const mockHistory = mockUseHistoryForStoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+			const mockHistory = renderHook(() => mockUseHistoryForStoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.store(mockHistory.balance);
+				transactionState = await depository.result.current.store(5);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test("must return success state of store transaction", () => {
 				assert.strictEqual(
@@ -88,24 +93,24 @@ describe("Depository", () => {
 			});
 
 			await t.test("depository balance must increase", () => {
-				assert.strictEqual(result.current.balance, 5);
+				assert.strictEqual(depository.result.current.balance, 5);
 			});
 
 			await t.test("history balance must decrease", () => {
-				assert.strictEqual(mockHistory.balance, 0);
+				assert.strictEqual(mockHistory.result.current.balance, 0);
 			});
 		});
 
 		test("store to depository zero value", async (t) => {
-			const mockHistory = mockUseHistoryForStoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+			const mockHistory = renderHook(() => mockUseHistoryForStoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.store(0);
+				transactionState = await depository.result.current.store(0);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test("must return zero param error state of transaction", () => {
 				assert.strictEqual(
@@ -115,24 +120,24 @@ describe("Depository", () => {
 			});
 
 			await t.test("depository balance shouldn't change", () => {
-				assert.strictEqual(result.current.balance, 0);
+				assert.strictEqual(depository.result.current.balance, 0);
 			});
 
 			await t.test("history balance shouldn't change", () => {
-				assert.strictEqual(mockHistory.balance, 5);
+				assert.strictEqual(mockHistory.result.current.balance, 5);
 			});
 		});
 
 		test("store to depository negative value", async (t) => {
-			const mockHistory = mockUseHistoryForStoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+			const mockHistory = renderHook(() => mockUseHistoryForStoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.store(-1);
+				transactionState = await depository.result.current.store(-1);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test(
 				"must return negative param error state of transaction",
@@ -145,24 +150,24 @@ describe("Depository", () => {
 			);
 
 			await t.test("depository balance shouldn't change", () => {
-				assert.strictEqual(result.current.balance, 0);
+				assert.strictEqual(depository.result.current.balance, 0);
 			});
 
 			await t.test("history balance shouldn't change", () => {
-				assert.strictEqual(mockHistory.balance, 5);
+				assert.strictEqual(mockHistory.result.current.balance, 5);
 			});
 		});
 
 		test("store to depository over balance value", async (t) => {
-			const mockHistory = mockUseHistoryForStoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+			const mockHistory = renderHook(() => mockUseHistoryForStoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.store(mockHistory.balance + 1);
+				transactionState = await depository.result.current.store(6);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test(
 				"must return over balance param error state of transaction",
@@ -175,11 +180,11 @@ describe("Depository", () => {
 			);
 
 			await t.test("depository balance shouldn't change", () => {
-				assert.strictEqual(result.current.balance, 0);
+				assert.strictEqual(depository.result.current.balance, 0);
 			});
 
 			await t.test("history balance shouldn't change", () => {
-				assert.strictEqual(mockHistory.balance, 5);
+				assert.strictEqual(mockHistory.result.current.balance, 5);
 			});
 		});
 	});
@@ -195,29 +200,35 @@ describe("Depository", () => {
 		];
 
 		function mockUseHistoryForRestoreTest(): UseHistoryReturn {
-			const history = mockHistoryForRestoreTest.slice();
+			const [history, setHistory] = useState(
+				mockHistoryForRestoreTest.slice(),
+			);
+
 			const balance = history.reduce((acc, row) => acc + row.change, 0);
 			const isHistoryParsed = "parsed";
 			const addHistoryRow = async ({
 				title,
 				change,
 			}: AddHistoryRowType): Promise<void> => {
-				history.push({ title, change, date: moment().format(DATE_FORMAT) });
+				setHistory((prev) => [
+					...prev,
+					{ title, change, date: moment().format(DATE_FORMAT) },
+				]);
 			};
 
 			return { historyRows: history, balance, isHistoryParsed, addHistoryRow };
 		}
 
 		test("restore from depository middle correct value", async (t) => {
-			const mockHistory = mockUseHistoryForRestoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+			const mockHistory = renderHook(() => mockUseHistoryForRestoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.restore(2);
+				transactionState = await depository.result.current.restore(2);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test("must return success state of restore transaction", () => {
 				assert.strictEqual(
@@ -227,24 +238,24 @@ describe("Depository", () => {
 			});
 
 			await t.test("depository balance must decrease", () => {
-				assert.strictEqual(result.current.balance, 3);
+				assert.strictEqual(depository.result.current.balance, 3);
 			});
 
 			await t.test("history balance must increase", () => {
-				assert.strictEqual(mockHistory.balance, 2);
+				assert.strictEqual(mockHistory.result.current.balance, 2);
 			});
 		});
 
 		test("restore from depository boundary correct value", async (t) => {
-			const mockHistory = mockUseHistoryForRestoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+			const mockHistory = renderHook(() => mockUseHistoryForRestoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.restore(5);
+				transactionState = await depository.result.current.restore(5);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test("must return success state of restore transaction", () => {
 				assert.strictEqual(
@@ -254,24 +265,24 @@ describe("Depository", () => {
 			});
 
 			await t.test("depository balance must decrease", () => {
-				assert.strictEqual(result.current.balance, 0);
+				assert.strictEqual(depository.result.current.balance, 0);
 			});
 
 			await t.test("history balance must increase", () => {
-				assert.strictEqual(mockHistory.balance, 5);
+				assert.strictEqual(mockHistory.result.current.balance, 5);
 			});
 		});
 
 		test("restore to depository zero value", async (t) => {
-			const mockHistory = mockUseHistoryForRestoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+			const mockHistory = renderHook(() => mockUseHistoryForRestoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.restore(0);
+				transactionState = await depository.result.current.restore(0);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test("must return zero param error state of transaction", () => {
 				assert.strictEqual(
@@ -281,24 +292,24 @@ describe("Depository", () => {
 			});
 
 			await t.test("depository balance shouldn't change", () => {
-				assert.strictEqual(result.current.balance, 5);
+				assert.strictEqual(depository.result.current.balance, 5);
 			});
 
 			await t.test("history balance shouldn't change", () => {
-				assert.strictEqual(mockHistory.balance, 0);
+				assert.strictEqual(mockHistory.result.current.balance, 0);
 			});
 		});
 
-		test("store to depository negative value", async (t) => {
-			const mockHistory = mockUseHistoryForRestoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+		test("restore from depository negative value", async (t) => {
+			const mockHistory = renderHook(() => mockUseHistoryForRestoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.restore(-1);
+				transactionState = await depository.result.current.restore(-1);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test(
 				"must return negative param error state of transaction",
@@ -311,24 +322,24 @@ describe("Depository", () => {
 			);
 
 			await t.test("depository balance shouldn't change", () => {
-				assert.strictEqual(result.current.balance, 5);
+				assert.strictEqual(depository.result.current.balance, 5);
 			});
 
 			await t.test("history balance shouldn't change", () => {
-				assert.strictEqual(mockHistory.balance, 0);
+				assert.strictEqual(mockHistory.result.current.balance, 0);
 			});
 		});
 
-		test("store to depository over balance value", async (t) => {
-			const mockHistory = mockUseHistoryForRestoreTest();
-			const { result } = renderHook(() => useDepository(mockHistory));
+		test("restore from depository over depository balance value", async (t) => {
+			const mockHistory = renderHook(() => mockUseHistoryForRestoreTest());
+			const depository = renderHook(() =>
+				useDepository(mockHistory.result.current),
+			);
 			let transactionState: DepositoryTransactionState;
 
 			await act(async () => {
-				transactionState = await result.current.store(mockHistory.balance + 1);
+				transactionState = await depository.result.current.restore(6);
 			});
-
-			mockHistory.balance = recalculateBalance(mockHistory.historyRows);
 
 			await t.test(
 				"must return over balance param error state of transaction",
@@ -341,11 +352,11 @@ describe("Depository", () => {
 			);
 
 			await t.test("depository balance shouldn't change", () => {
-				assert.strictEqual(result.current.balance, 5);
+				assert.strictEqual(depository.result.current.balance, 5);
 			});
 
 			await t.test("history balance shouldn't change", () => {
-				assert.strictEqual(mockHistory.balance, 0);
+				assert.strictEqual(mockHistory.result.current.balance, 0);
 			});
 		});
 	});
