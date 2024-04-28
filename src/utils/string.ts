4ca438acf055091b667681413ec59fb138d101d7
dromse <dromasko4@gmail.com>
2024-04-28 23:12:38 +0300

    add tsdoc description to singularOrPlural function

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -4,6 +4,25 @@ type SingularOrPlural = {
 	plural?: string;
 };
 
+/**
+ * Return amount and string representations of enumeration
+ *
+ * @example
+ *
+ * - Without plural
+ * ```javascript
+ * ({amount: 0, singular: 'coin'}) => "0 coins"
+ * ({amount: 1, singular: 'coin'}) => "1 coin"
+ * ({amount: 2, singular: 'coin'}) => "2 coins"
+ * ```
+ *
+ * - With plural
+ * ```javascript
+ * ({amount: 0, singular: 'leaf', plural: 'leavies'}) => "0 leavies"
+ * ({amount: 1, singular: 'leaf', plural: 'leavies'}) => "1 leaf"
+ * ({amount: 2, singular: 'leaf', plural: 'leavies'}) => "2 leavies"
+ * ```
+ */
 export function singularOrPlural({
 	amount,
 	singular,
@@ -15,3 +34,4 @@ export function singularOrPlural({
 
 	return `${amount} ${amount > 1 || amount === 0 ? singular + "s" : singular}`;
 }
+
