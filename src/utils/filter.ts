fffc3844847f7adf2f0f3d37686401f6c9e75927
dromse <dromasko4@gmail.com>
2024-05-26 19:07:39 +0300

    fix #42: show task with counter if status is 'doing'

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -8,112 +8,115 @@ import { generatePastDaysArray, getAmountOfPastDays } from "./date";
  */
 export const bySearch =
 	(searchFilter: string) =>
-	(task: Task): boolean =>
-		task.body
-			? task.body.toLowerCase().includes(searchFilter.toLowerCase())
-			: true;
+		(task: Task): boolean =>
+			task.body
+				? task.body.toLowerCase().includes(searchFilter.toLowerCase())
+				: true;
 
 /**
  * Use currying to set tag filters and return callback to call in filter
  */
 export const byTag =
 	(tagFilter: string, hasOnlyThisTags: boolean) =>
-	(task: Task): boolean => {
-		if (tagFilter.length === 0) {
-			return true;
-		}
+		(task: Task): boolean => {
+			if (tagFilter.length === 0) {
+				return true;
+			}
 
-		const tags = tagFilter
-			.split(",")
-			.map((tag) => tag.trim())
-			.filter((trimmedTag) => trimmedTag !== "")
-			.map((tag) => "#" + tag);
+			const tags = tagFilter
+				.split(",")
+				.map((tag) => tag.trim())
+				.filter((trimmedTag) => trimmedTag !== "")
+				.map((tag) => "#" + tag);
 
-		if (hasOnlyThisTags) {
-			return tags.every((tag) => task.lineContent.includes(tag));
-		} else {
-			return tags.some((tag) => task.lineContent.includes(tag));
-		}
-	};
+			if (hasOnlyThisTags) {
+				return tags.every((tag) => task.lineContent.includes(tag));
+			} else {
+				return tags.some((tag) => task.lineContent.includes(tag));
+			}
+		};
 
 /**
  * Use currying to set note filters and return callback to call in filter
  */
 export const byNote =
 	(noteFilter: string, isFromCurrentNote: boolean, workspace: Workspace) =>
-	(task: Task): boolean => {
-		if (isFromCurrentNote) {
-			const activeNote = workspace.getActiveFile();
+		(task: Task): boolean => {
+			if (isFromCurrentNote) {
+				const activeNote = workspace.getActiveFile();
 
-			if (activeNote) {
-				return task.path === activeNote.path;
+				if (activeNote) {
+					return task.path === activeNote.path;
+				}
+
+				return false;
 			}
 
-			return false;
-		}
+			if (noteFilter === "") {
+				return true;
+			}
 
-		if (noteFilter === "") {
-			return true;
-		}
-
-		return task.path === noteFilter + ".md";
-	};
+			return task.path === noteFilter + ".md";
+		};
 
 /**
  * Use currying to set status filter and return callback to call in filter
  */
 export const byStatus =
 	(statusFilter: string) =>
-	(task: Task): boolean => {
-		if (statusFilter === "all") {
-			return true;
-		}
+		(task: Task): boolean => {
+			if (statusFilter === "all") {
+				return true;
+			}
 
-		if (!task.status) {
+			if (!task.status) {
+				return false;
+			}
+
+			if (statusFilter === task.status) {
+				return true;
+			}
+
 			return false;
-		}
-
-		if (statusFilter === task.status) {
-			return true;
-		}
-
-		return false;
-	};
+		};
 
 /**
  * Use currying to set history rows and return callback to call in filter
  */
 export const byToday =
 	(historyRows: ReadonlyArray<HistoryRow>) =>
-	(task: Task): boolean => {
-		if (!task.every) {
-			return false;
-		}
-
-		let amountOfDaysToShowAgain = getAmountOfPastDays(task.every);
-
-		if (!amountOfDaysToShowAgain) {
-			return false;
-		}
-
-		const pastDaysList = generatePastDaysArray(amountOfDaysToShowAgain);
-
-		const dateRange = historyRows
-			.filter((row) => pastDaysList.includes(row.date.split(" ")[0]))
-			.map((row) => row.title);
-
-		const isTaskNotAppearInPastDays = !dateRange.includes(task.body);
-
-		if (task.counter) {
-			const isCounterNotFull = task.counter.current !== task.counter.goal;
-
-			if (isCounterNotFull) {
-				return true;
+		(task: Task): boolean => {
+			if (!task.every) {
+				return false;
 			}
-		}
 
-		return isTaskNotAppearInPastDays;
-	};
+			let amountOfDaysToShowAgain = getAmountOfPastDays(task.every);
+
+			if (!amountOfDaysToShowAgain) {
+				return false;
+			}
+
+			const pastDaysList = generatePastDaysArray(amountOfDaysToShowAgain);
+
+			const dateRange = historyRows
+				.filter((row) => pastDaysList.includes(row.date.split(" ")[0]))
+				.map((row) => row.title);
+
+			const isTaskNotAppearInPastDays = !dateRange.some(
+				(title) => title === task.body,
+			);
+
+			if (task.counter && task.status === "doing") {
+				console.log(task.status);
+				const isCounterNotFull = task.counter.current !== task.counter.goal;
+
+				if (isCounterNotFull) {
+					return true;
+				}
+			}
+
+			return isTaskNotAppearInPastDays;
+		};
 
 export const byRecurrance = (task: Task): boolean =>
 	task.every ? true : false;
