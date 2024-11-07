c2ca86ce25b0b05977efb2a1747d97ea2308535c
dromse <dromasko4@gmail.com>
2024-11-07 23:43:01 +0200

    add icons to tabs

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -1,21 +1,45 @@
 import FiltersProvider from "@providers/FiltersProvider";
 import SortingProvider from "@providers/SortingProvider";
+import { BookCheck, CakeSlice, History as HistoryIcon } from "lucide-react";
 import React from "react";
 import Tabs from "./reusable/Tabs";
 import { History, Rewards, Tasks } from "./tabs";
 
 export const UI = (): React.JSX.Element => {
-	const tabs = [
-		{ title: "Tasks", view: Tasks },
-		{ title: "Rewards", view: Rewards },
-		{ title: "History", view: History },
-	];
+    const tabs = [
+        {
+            title: (
+                <>
+                    <BookCheck className='p-1' /> Tasks
+                </>
+            ),
+            view: Tasks,
+        },
+        {
+            title: (
+                <>
+                    <CakeSlice className='p-1' />
+                    Rewards
+                </>
+            ),
+            view: Rewards,
+        },
+        {
+            title: (
+                <>
+                    <HistoryIcon className='p-1' />
+                    History
+                </>
+            ),
+            view: History,
+        },
+    ];
 
-	return (
-		<FiltersProvider>
-			<SortingProvider>
-				<Tabs tabs={tabs} />
-			</SortingProvider>
-		</FiltersProvider>
-	);
+    return (
+        <FiltersProvider>
+            <SortingProvider>
+                <Tabs tabs={tabs} />
+            </SortingProvider>
+        </FiltersProvider>
+    );
 };
--- src/components/reusable/Tabs/index.tsx
+++ src/components/reusable/Tabs/index.tsx
@@ -2,34 +2,35 @@ import React, { useState } from "react";
 import styles from "./styles.module.css";
 
 type Tab = {
-	title: string;
-	view: React.ComponentType<unknown>;
+    title: string | React.JSX.Element;
+    view: React.ComponentType<unknown>;
 };
 
 type Props = {
-	tabs: Array<Tab>;
+    tabs: Array<Tab>;
 };
 
 export default function Tabs(props: Props): React.JSX.Element {
-	const { tabs } = props;
-	const [activeTab, setActiveTab] = useState<Tab>(tabs[0]);
+    const { tabs } = props;
+    const [activeTab, setActiveTab] = useState<Tab>(tabs[0]);
 
-	return (
-		<div>
-			<ul className={styles.tabs}>
-				{tabs.map((tab) => (
-					<li className='list-item' key={tab.title}>
-						<button
-							id={`${tab.title === activeTab.title ? styles.active : ""}`}
-							onClick={() => setActiveTab(tab)}
-						>
-							{tab.title}
-						</button>
-					</li>
-				))}
-			</ul>
+    return (
+        <div>
+            <ul className={styles.tabs}>
+                {tabs.map((tab) => (
+                    <li className='list-item' key={String(tab.title)}>
+                        <button
+                            id={`${tab.title === activeTab.title ? styles.active : ""}`}
+                            onClick={() => setActiveTab(tab)}
+                            className='pl-1 pr-2'
+                        >
+                            {tab.title}
+                        </button>
+                    </li>
+                ))}
+            </ul>
 
-			<activeTab.view />
-		</div>
-	);
+            <activeTab.view />
+        </div>
+    );
 }
